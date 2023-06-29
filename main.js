e4acf2a9d9c1761fba0dc85143e7e3b695985c20
Nicholas Lor <bobbylor00@gmail.com>
2023-06-29 01:26:45 -0600

    mvp, verticla scroll snap

---
+++ main.js
@@ -0,0 +1,22 @@
+import './style.css'
+
+
+function generateColorCode() {
+  // Generate random color code
+  var colorCode = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+  return colorCode;
+}
+
+// Usage example
+var color = generateColorCode();
+console.log(color);
+const init = function(){
+let items = document.querySelectorAll('section');
+for (let i = 0; i < items.length; i++){
+
+  items[i].style.background = generateColorCode();
+}
+}
+init();
+
