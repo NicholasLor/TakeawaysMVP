b41a2f1636a4e9eb702c2ef556809593c7c74459
Nicholas Lor <bobbylor00@gmail.com>
2023-07-03 09:59:34 -0600

    swiper library

---
+++ main.js
@@ -0,0 +1,30 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation],
+    direction: 'vertical',
+    loop: true,
+
+      // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  });
